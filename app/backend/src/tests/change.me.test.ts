f82cd1732c89b01ac0d893db052afd5d3c2b7acf
Everson <eversond14s@gmail.com>
2023-06-14 22:49:07 -0300

    create test getAll and getId

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -2,17 +2,20 @@ import * as sinon from 'sinon';
 import * as chai from 'chai';
 // @ts-ignore
 import chaiHttp = require('chai-http');
+import TeamModel from '../../src/database/models/TeamModel';
+import ITeams from '../Interfaces/Teams';
 
 import { app } from '../app';
 import Example from '../database/models/ExampleModel';
 
 import { Response } from 'superagent';
+import listTeamsMock from './mock/listTeamsMock';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Seu teste', () => {
+describe('tests', () => {
   /**
    * Exemplo do uso de stubs com tipos
    */
@@ -39,7 +42,27 @@ describe('Seu teste', () => {
   //   expect(...)
   // });
 
-  it('Seu sub-teste', () => {
-    expect(false).to.be.eq(true);
+  it('function getAll return all teams', async () => {
+    sinon.stub(TeamModel, 'findAll').resolves(listTeamsMock as any)
+    const {status, body} = await chai.request(app)
+    .post('/teams')
+    .send()
+
+    expect(status).to.equal(200);
+    expect(body).to.deep.equal(listTeamsMock);
   });
+
+  it('function getId return team', async () => {
+    sinon.stub(TeamModel, 'findOne').resolves(listTeamsMock[1] as any)
+    const {status, body} = await chai.request(app)
+    .post('/teams/1')
+    .send()
+
+    expect(status).to.equal(200);
+    expect(body).to.deep.equal(listTeamsMock[1]);
+  })
+
+  afterEach(sinon.restore);
+
+
 });
---
+++ app/backend/src/tests/mock/listTeamsMock.ts
@@ -0,0 +1,22 @@
+export default [
+  {
+    id: 1,
+    team_name: 'São Paulo',
+  },
+  {
+    id: 2,
+    team_name: 'Santos',
+  },
+  {
+    id: 3,
+    team_name: 'Palmeiras',
+  },
+  {
+    id: 4,
+    team_name: 'Flamengo',
+  },
+  {
+    id: 5,
+    team_name: 'Grêmio',
+  },
+]
